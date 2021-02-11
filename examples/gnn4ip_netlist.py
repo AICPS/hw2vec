c81dbc13fd924016126a8fb0cdb6f584b746b344
Sarah <11711130@mail.sustech.edu.cn>
2021-02-10 17:23:02 -0800

    add config with yaml, still need refactor

--- examples/gnn4ip_netlist.py
+++ examples/gnn4ip_netlist.py
@@ -7,6 +7,8 @@ from argparse import ArgumentParser, RawTextHelpFormatter
 from pathlib import Path
 from hw2vec.graph2vec.trainers import *
 from hw2vec.hw2graph import *
+import itertools
+from config import *
 
 
 class Config:
@@ -164,33 +166,34 @@ class Config:
 
 
 if __name__ == "__main__":
-    parser = ArgumentParser(description="scripts for generating datasets", formatter_class=RawTextHelpFormatter)
-    parser.add_argument('--input_path', type=str, default="../input/synthesis_data/lane-change/", help="Path to code directory.")
-    parser.add_argument('--learning_rate', default=0.001, type=float, help='The initial learning rate for GCN.')
-    parser.add_argument('--seed', type=int, default=random.randint(0,2**32), help='Random seed.')
-    parser.add_argument('--epochs', type=int, default=200, help='Number of epochs to train.')
-    parser.add_argument('--weight_decay', type=float, default=5e-4, help='Weight decay (L2 loss on parameters).')
-    parser.add_argument('--hidden', type=int, default=200, help='Number of hidden units.')
-    parser.add_argument('--dropout', type=float, default=0.5, help='Dropout rate (1 - keep probability).')
-    parser.add_argument('--embed_dim', type=int, default=32, help="The dimension of graph embeddings.")
-    parser.add_argument('--batch_size', type=int, default=4, help='Number of graphs in a batch.')
-    parser.add_argument('--device', type=str, default="cpu", help='The device to run on models (cuda or cpu) cpu in default.')
-    parser.add_argument('--model', type=str, default="gcn", help="Model to be used intrinsically.")
-    parser.add_argument('--num_layers', type=int, default=5, help="Number of layers in the neural network.")
-    parser.add_argument('--test_step', type=int, default=10, help="The interval between mini evaluation along the training process.")
-    parser.add_argument('--pooling_type', type=str, default="sagpool", help="Graph pooling type.")
-    parser.add_argument('--readout_type', type=str, default="max", help="Readout type.")
-    parser.add_argument('--splitted', type=bool, default=False, help="Indicating whether the target path has been splitted.")
-    parser.add_argument('--ratio', type=float, default=0.8, help="Dataset splitting ratio")
-    parser.add_argument('--poolratio', type=float, default=0.75, help="Ratio for graph pooling.")
-    parser.add_argument('--debug', type=bool, default=False, help="Train on all pairs in debug mode.")
-    parser.add_argument('--raw_dataset_path', type=str, default='../data/IP-datasets/data_DFG/', help="Path to raw dataset for parsing if no precache.")
-    parser.add_argument('--pkl_path', type=str, default='./dataset_rtl.pkl', help="Path to hardware graphs for parsing.")
-    parser.add_argument('--dataset_name', type=str, default='RTL', help="name of hardware design languages." )
-    parser.add_argument('--debug', type=lambda x: (str(x).lower() == 'true'), default=True, help='debug mode.')
+    # parser = ArgumentParser(description="scripts for generating datasets", formatter_class=RawTextHelpFormatter)
+    # parser.add_argument('--input_path', type=str, default="../input/synthesis_data/lane-change/", help="Path to code directory.")
+    # parser.add_argument('--learning_rate', default=0.001, type=float, help='The initial learning rate for GCN.')
+    # parser.add_argument('--seed', type=int, default=random.randint(0,2**32), help='Random seed.')
+    # parser.add_argument('--epochs', type=int, default=200, help='Number of epochs to train.')
+    # parser.add_argument('--weight_decay', type=float, default=5e-4, help='Weight decay (L2 loss on parameters).')
+    # parser.add_argument('--hidden', type=int, default=200, help='Number of hidden units.')
+    # parser.add_argument('--dropout', type=float, default=0.5, help='Dropout rate (1 - keep probability).')
+    # parser.add_argument('--embed_dim', type=int, default=32, help="The dimension of graph embeddings.")
+    # parser.add_argument('--batch_size', type=int, default=4, help='Number of graphs in a batch.')
+    # parser.add_argument('--device', type=str, default="cpu", help='The device to run on models (cuda or cpu) cpu in default.')
+    # parser.add_argument('--model', type=str, default="gcn", help="Model to be used intrinsically.")
+    # parser.add_argument('--num_layers', type=int, default=5, help="Number of layers in the neural network.")
+    # parser.add_argument('--test_step', type=int, default=10, help="The interval between mini evaluation along the training process.")
+    # parser.add_argument('--pooling_type', type=str, default="sagpool", help="Graph pooling type.")
+    # parser.add_argument('--readout_type', type=str, default="max", help="Readout type.")
+    # parser.add_argument('--splitted', type=bool, default=False, help="Indicating whether the target path has been splitted.")
+    # parser.add_argument('--ratio', type=float, default=0.8, help="Dataset splitting ratio")
+    # parser.add_argument('--poolratio', type=float, default=0.75, help="Ratio for graph pooling.")
+    # parser.add_argument('--debug', type=bool, default=False, help="Train on all pairs in debug mode.")
+    # parser.add_argument('--raw_dataset_path', type=str, default='../data/IP-datasets/data_DFG/', help="Path to raw dataset for parsing if no precache.")
+    # parser.add_argument('--pkl_path', type=str, default='./dataset_rtl.pkl', help="Path to hardware graphs for parsing.")
+    # parser.add_argument('--dataset_name', type=str, default='RTL', help="name of hardware design languages." )
+    # parser.add_argument('--debug', type=lambda x: (str(x).lower() == 'true'), default=True, help='debug mode.')
 
-    args_parsed = parser.parse_args(sys.argv[1:])
-    cfg = Config(args_parsed)
+    # args_parsed = parser.parse_args(sys.argv[1:])
+    # cfg = Config(args_parsed)
+    cfg = YamlConfig("config.yaml")
 
     '''
         Commands for running the experiments:
