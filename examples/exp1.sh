55c1013398d764f99f62da24e4cc9bd78b877ca3
Louis Yu <louis29418401@gmail.com>
2021-04-21 16:43:12 -0700

    update script.

--- examples/exp1.sh
+++ examples/exp1.sh
@@ -1,23 +1,23 @@
-echo python use_case_2.py --yaml_path ../hw2vec/configs/AST-IP-NetList.yaml
-python use_case_2.py --yaml_path ../hw2vec/configs/AST-IP-NetList.yaml
+echo python graph_processor.py --yaml_path ../hw2vec/configs/AST-IP-NetList.yaml
+python graph_processor.py --yaml_path ../hw2vec/configs/AST-IP-NetList.yaml
 
-echo python use_case_2.py --yaml_path ../hw2vec/configs/AST-IP-RTL.yaml
-python use_case_2.py --yaml_path ../hw2vec/configs/AST-IP-RTL.yaml
+echo python graph_processor.py --yaml_path ../hw2vec/configs/AST-IP-RTL.yaml
+python graph_processor.py --yaml_path ../hw2vec/configs/AST-IP-RTL.yaml
 
-echo python use_case_2.py --yaml_path ../hw2vec/configs/AST-TJ-NetList.yaml
-python use_case_2.py --yaml_path ../hw2vec/configs/AST-TJ-NetList.yaml
+echo python graph_processor.py --yaml_path ../hw2vec/configs/AST-TJ-NetList.yaml
+python graph_processor.py --yaml_path ../hw2vec/configs/AST-TJ-NetList.yaml
 
-echo python use_case_2.py --yaml_path ../hw2vec/configs/AST-TJ-RTL.yaml
-python use_case_2.py --yaml_path ../hw2vec/configs/AST-TJ-RTL.yaml
+echo python graph_processor.py --yaml_path ../hw2vec/configs/AST-TJ-RTL.yaml
+python graph_processor.py --yaml_path ../hw2vec/configs/AST-TJ-RTL.yaml
 
-echo python use_case_2.py --yaml_path ../hw2vec/configs/DFG-IP-NetList.yaml
-python use_case_2.py --yaml_path ../hw2vec/configs/DFG-IP-NetList.yaml
+echo python graph_processor.py --yaml_path ../hw2vec/configs/DFG-IP-NetList.yaml
+python graph_processor.py --yaml_path ../hw2vec/configs/DFG-IP-NetList.yaml
 
-echo python use_case_2.py --yaml_path ../hw2vec/configs/DFG-IP-RTL.yaml
-python use_case_2.py --yaml_path ../hw2vec/configs/DFG-IP-RTL.yaml
+echo python graph_processor.py --yaml_path ../hw2vec/configs/DFG-IP-RTL.yaml
+python graph_processor.py --yaml_path ../hw2vec/configs/DFG-IP-RTL.yaml
 
-echo python use_case_2.py --yaml_path ../hw2vec/configs/DFG-TJ-NetList.yaml
-python use_case_2.py --yaml_path ../hw2vec/configs/DFG-TJ-NetList.yaml
+echo python graph_processor.py --yaml_path ../hw2vec/configs/DFG-TJ-NetList.yaml
+python graph_processor.py --yaml_path ../hw2vec/configs/DFG-TJ-NetList.yaml
 
-echo python use_case_2.py --yaml_path ../hw2vec/configs/DFG-TJ-RTL.yaml
-python use_case_2.py --yaml_path ../hw2vec/configs/DFG-TJ-RTL.yaml
+echo python graph_processor.py --yaml_path ../hw2vec/configs/DFG-TJ-RTL.yaml
+python graph_processor.py --yaml_path ../hw2vec/configs/DFG-TJ-RTL.yaml
