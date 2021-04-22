5ce5442d67a3237ef6f3fa9babe25215e4b3010a
Louis Yu <louis29418401@gmail.com>
2021-04-21 22:53:14 -0700

    opt

--- examples/graph_processer.py
+++ examples/graph_processer.py
@@ -17,28 +17,27 @@ import pickle
 
 
 def process_graphs(cfg):
-    data_proc = DataProcessor(cfg)
-
+    # step1: process graph with hw2graph     
     nx_graphs = []
     for verilog_path in glob("%s/**/topModule.v" % str(cfg.raw_dataset_path), recursive=True):
         print("Reading ", verilog_path)
         hw2graph = HW2GRAPH(cfg, verilog_path)
         hardware_graph = hw2graph.process() #TODO: not implemented for CFG.
         nx_graphs.append((hardware_graph, verilog_path)) 
-
+    
+    # step2: use data_proc to create the dataset.
+    data_proc = DataProcessor(cfg)
     for hw_graph, verilog_path in nx_graphs:
         data_proc.normalize(hw_graph, normalize=cfg.NORMALIZATION, graph_format=cfg.graph_type)
         data = from_networkx(hw_graph)
         data.folder_name = verilog_path
         data_proc.append_graph_data(data)
     
-    import pdb; pdb.set_trace()
-    
+    # step3: cache the whole data_proc.
     with open(cfg.data_pkl_path, 'wb+') as f:
         pickle.dump(data_proc, f)
 
 
 if __name__ == '__main__': 
     cfg = Config(sys.argv[1:])
-
     process_graphs(cfg)
