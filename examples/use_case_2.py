851b2c01d42eba7bfdd7809b53344b719f3bc94f
Louis Yu <louis29418401@gmail.com>
2021-05-11 16:35:13 -0700

    update use-case-2.py

--- examples/use_case_2.py
+++ examples/use_case_2.py
@@ -1,65 +1,55 @@
 import os, sys
 sys.path.append(os.path.dirname(sys.path[0]))
 from hw2vec.config import Config
-from hw2vec.app import GNN4TJ
 from hw2vec.hw2graph import *
-import pickle
-import torch
-from glob import glob
-from time import time
 
+
+cfg = Config(sys.argv[1:])
+
+''' prepare graph data '''
+if not cfg.data_pkl_path.exists():
+    ''' converting graph using hw2graph '''
+    nx_graphs = []
+    hw2graph = HW2GRAPH(cfg)
+    for hw_project_path in hw2graph.find_hw_project_folders():
+        hw_graph = hw2graph.code2graph(hw_project_path)
+        nx_graphs.append(hw_graph)
+    
+    data_proc = DataProcessor(cfg)
+    for hw_graph in nx_graphs:
+        data_proc.process(hw_graph)
+    data_proc.cache_graph_data(cfg.data_pkl_path)
+    
+else:
+    ''' reading graph data from cache '''
+    data_proc = DataProcessor(cfg)
+    data_proc.read_graph_data_from_cache(cfg.data_pkl_path)
+    
+
+''' prepare dataset '''
 TROJAN = 1
 NON_TROJAN = 0
 
-def process_graph(folder_path, profile=True):
-    hw2graph = HW2GRAPH(cfg)
-    
-    if profile:
-        start = time()
-
-    if "topModule.v" not in [Path(x).name for x in glob("%s/*.v"%str(folder_path))]:
-        code_path = hw2graph.preprocess(folder_path)
-    else:
-        code_path = folder_path / "topModule.v"
-
-    hw_graph = hw2graph.process(str(code_path))
-    
-    if profile:
-        end = time()
-        print(str(code_path), ",", len(hw_graph.nodes), ",", len(hw_graph.edges), ",", end-start)
-
-    return hw_graph
-
-cfg = Config(sys.argv[1:])
-data_proc = DataProcessor(cfg)
-if cfg.data_pkl_path.exists():
-    data_proc.read_graph_data_from_cache(cfg.data_pkl_path)
-else:
-    for project in data_proc.find_projects():
-        hw_graph = process_graph(project)
-        data_proc.process(hw_graph)
-
-    data_proc.cache_graph_data(cfg.data_pkl_path)
-
-for data in data_proc.graph_data:
+all_graphs = data_proc.graph_data
+for data in all_graphs:
     if "TjFree" == data.hw_type:
         data.label = NON_TROJAN
     else:
         data.label = TROJAN
 
-all_graphs = data_proc.graph_data
 train_graphs, test_graphs = data_proc.get_graphs()
-class_weights = data_proc.get_class_weights(train_graphs)
-
 train_loader = DataLoader(train_graphs, shuffle=True, batch_size=cfg.batch_size)
-test_loader  = DataLoader(test_graphs, shuffle=True, batch_size=1)
+valid_loader = DataLoader(test_graphs, shuffle=True, batch_size=1)
 
+''' training '''
+class_weights = data_proc.get_class_weights(train_graphs)
 trainer = GraphTrainer(cfg, class_weights=class_weights)
 trainer.build()
 # if model_path is not None:
     # trainer.load_saved_model(model_path)
-trainer.train(train_loader, test_loader)
-trainer.evaluate(cfg.epochs, train_loader, test_loader)
+trainer.train(train_loader, valid_loader)
 
+''' evaluating and inspecting '''
+trainer.evaluate(cfg.epochs, train_loader, valid_loader)
 vis_loader = DataLoader(all_graphs, batch_size=1)
 trainer.visualize_embeddings(vis_loader, "./")
