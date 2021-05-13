4b68547607afc9b7269def542c9f774d0cddaf91
Tommy Nguyen <47965445+tthenguyen@users.noreply.github.com>
2021-05-13 15:55:23 -0700

    delete unzipped toy datasets.

--- assets/data/IP-RTL-toy/encoder8to3/encoder8to3_1/topModule.v
+++
@@ -1,7 +0,0 @@
-module top (O,I);
-output [2:0] O;
-input [7:0] I;
-or (O[0],I[1],I[3],I[5],I[7]);
-or (O[1],I[2],I[3],I[6],I[7]);
-or (O[2],I[4],I[5],I[6],I[7]);
-endmodule 
--- assets/data/IP-RTL-toy/encoder8to3/encoder8to3_2/topModule.v
+++
@@ -1,8 +0,0 @@
-module top (I, Y);
-input [7:0] I;//defines inputs
-output [2:0]Y;//defines outputs
-assign Y[2]=I[4] | I[5] | I[6] | I[7];
-assign Y[1]=I[2] | I[3] | I[6] | I[7];
-assign Y[0]=I[1] | I[3] | I[5] | I[7];
-//determines assignments for Y according to karno table
-endmodule
